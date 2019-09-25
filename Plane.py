9d5696a139ae142b461febd5419fe972ce484ff7
lukvidator <vlad.muhin1999@mail.ru>
2019-09-25 23:19:51 +0300

    Fixed Plane.equation

--- Plane.py
+++ Plane.py
@@ -6,7 +6,7 @@ from Vector import Vector
 
 
 class Plane:
-    def __init__(self, point, *vectors):
+    def __init__(self, point, vectors):
         if len(point) == len(vectors[0]) and np.array(vectors).shape == 2:
             self._point = np.array(point, dtype=np.float64)
             self._vectors = np.array(vectors, dtype=np.float64)
@@ -23,17 +23,16 @@ class Plane:
         return cls.from_coefficients(extract_coefs(equation, variables))
 
     @classmethod
-    def from_points(cls, *points):
+    def from_points(cls, points):
         return cls(points[0], [Vector(points[0], point) for point in points[1:]])
 
     @classmethod
     def from_point_and_normal(cls, point, normal):
         if isinstance(point, Point) and isinstance(normal, Vector):
-            return cls(np.append(normal, -np.dot(point, normal)))
+            return cls.from_coefficients(np.append(normal, -np.dot(point, normal)))
         else:
             raise WrongTypeException(f"Can't create {cls.__name__} with {(point, normal)}")
 
-    @property
     def coefficients(self):
         matrix = np.array([self._point, self._vectors])    # creating main matrix
         dim = len(self._point)    # getting the space dimension
@@ -48,16 +47,15 @@ class Plane:
         return coefficients
 
     def equation(self, var=None):
-        coefficients = self.coefficients
+        coefficients = self.coefficients()
         if not var:
             equation = \
                 ''.join([str(coef) + "*x" + str(i + 1) if coef != 0. else "" for i, coef in enumerate(coefficients[:-1])])
             equation += str(coefficients[-1]) if coefficients[-1] != 0. else ""
             equation.replace("+-", "-")
         elif len(var) == len(coefficients) - 1:
-            var = zip(coefficients, var)
             equation = \
-                ''.join([str(var[0]) + "*" + var[1] if var[0] != 0. else "" for pair in var])
+                ''.join([str(pair[0]) + "*" + pair[1] if pair[0] != 0. else "" for pair in zip(coefficients, var)])
             equation += str(coefficients[-1]) if coefficients[-1] != 0. else ""
             equation.replace("+-", "-")
         else:
@@ -66,7 +64,7 @@ class Plane:
         return equation
 
     def normal(self):
-        return Vector(self.coefficients[:-1])
+        return Vector(self.coefficients()[:-1])
 
     def dim(self):
         return len(self._point)
@@ -75,6 +73,17 @@ class Plane:
     def vectors(self):
         return self._vectors
 
+    @vectors.setter
+    def vectors(self, vectors):
+        if np.array(vectors).shape == 2:
+            self._vectors = np.array(vectors, dtype=np.float64)
+        else:
+            raise WrongDimensionException("Can't set vectors with different dimensions")
+
     @property
     def point(self):
         return self._point
+
+    @point.setter
+    def point(self, point):
+        self._point = np.array(point, dtype=np.float64)
