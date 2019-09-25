a60638f97d71c08da6429f43a6b1d028371520e0
lukvidator <vlad.muhin1999@mail.ru>
2019-09-25 23:05:39 +0300

    Restructed Plane class

--- Plane.py
+++ Plane.py
@@ -6,54 +6,59 @@ from Vector import Vector
 
 
 class Plane:
-    def __init__(self, *args):
-        types = [type(arg) for arg in args]
-
-        if len(args) == 1:
-            if type(args[0]) in (tuple, list, np.ndarray):
-                self.coefficients = np.array(args[0], dtype=np.float64)
-            else:
-                raise WrongTypeException(f"Can't create {type(self).__name__} with {args}")
-        elif len(args) == 2:
-            if types[0] == str and types[1] in (str, tuple, list):
-                self._coefs = extract_coefs(*args)
-            elif types == [Point, Vector] or types == [np.ndarray, np.ndarray]:
-                self._coefs = np.insert(args[1], len(args[1]), -np.dot(*args))
-            else:
-                raise WrongTypeException(f"Can't create {type(self).__name__} with {args}")
-        elif len(args) == 3:
-            if types == [Point, Vector, Vector]:
-                self._coefs = None    # TODO: implement Plane.__init__ for [Point, Vector, Vector]
-            elif types == [Point, Point, Point]:
-                self._point = args[0]
-                self._vectors = [Vector(args[0], args[1]), Vector(args[0], args[2])]
-            else:
-                raise WrongTypeException(f"Can't create {type(self).__name__} with {args}")
+    def __init__(self, point, *vectors):
+        if len(point) == len(vectors[0]) and np.array(vectors).shape == 2:
+            self._point = np.array(point, dtype=np.float64)
+            self._vectors = np.array(vectors, dtype=np.float64)
         else:
-            raise WrongTypeException(
-                f"{type(self).__name__} __init__ requires 1, 2 or 3 positional arguments but {len(args)} where given"
-            )
+            raise WrongDimensionException(f"Can't init {self.__class__} with args of different dimensions")
+
+    @classmethod
+    def from_coefficients(cls, coefficients):
+        points = find_three_plane_points(coefficients)
+        return cls(points[0], [point - points[0] for point in points[1:]])
+
+    @classmethod
+    def from_equation(cls, equation, variables):
+        return cls.from_coefficients(extract_coefs(equation, variables))
+
+    @classmethod
+    def from_points(cls, *points):
+        return cls(points[0], [Vector(points[0], point) for point in points[1:]])
+
+    @classmethod
+    def from_point_and_normal(cls, point, normal):
+        if isinstance(point, Point) and isinstance(normal, Vector):
+            return cls(np.append(normal, -np.dot(point, normal)))
+        else:
+            raise WrongTypeException(f"Can't create {cls.__name__} with {(point, normal)}")
 
     @property
     def coefficients(self):
-        return self._coefs
+        matrix = np.array([self._point, self._vectors])    # creating main matrix
+        dim = len(self._point)    # getting the space dimension
 
-    @coefficients.setter
-    def coefficients(self, coefs):
-        assert type(coefs) in (np.ndarray, list, tuple)
-        self._coefs = np.array(coefs, dtype=np.float64)
+        coefficients = np.array(
+            [(-1 if i % 2 else 1) * np.linalg.det(
+                matrix[np.ix_(range(1, dim), [j for j in range(0, dim) if j != i])]
+            ) for i in range(0, dim)])    # creating equation coefficients
+
+        np.append(coefficients, -np.linalg.det(matrix))    # adding free one
+
+        return coefficients
 
     def equation(self, var=None):
+        coefficients = self.coefficients
         if not var:
             equation = \
-                ''.join([str(coef) + "*x" + str(i + 1) if coef != 0. else "" for i, coef in enumerate(self._coefs[:-1])])
-            equation += str(self._coefs[-1]) if self._coefs[-1] != 0. else ""
+                ''.join([str(coef) + "*x" + str(i + 1) if coef != 0. else "" for i, coef in enumerate(coefficients[:-1])])
+            equation += str(coefficients[-1]) if coefficients[-1] != 0. else ""
             equation.replace("+-", "-")
-        elif len(var) == len(self._coefs) - 1:
-            var = zip(self._coefs, var)
+        elif len(var) == len(coefficients) - 1:
+            var = zip(coefficients, var)
             equation = \
                 ''.join([str(var[0]) + "*" + var[1] if var[0] != 0. else "" for pair in var])
-            equation += str(self._coefs[-1]) if self._coefs[-1] != 0. else ""
+            equation += str(coefficients[-1]) if coefficients[-1] != 0. else ""
             equation.replace("+-", "-")
         else:
             raise WrongTypeException(f"Can't create equation using {var}")
@@ -61,8 +66,15 @@ class Plane:
         return equation
 
     def normal(self):
-        return Vector(self._coefs[:-1])
+        return Vector(self.coefficients[:-1])
 
+    def dim(self):
+        return len(self._point)
+
+    @property
     def vectors(self):
-        points = find_three_plane_points(self._coefs)
-        return [Vector(points[0], points[1]), Vector(points[0], points[2])]
+        return self._vectors
+
+    @property
+    def point(self):
+        return self._point
