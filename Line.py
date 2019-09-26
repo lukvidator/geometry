e0f696c5a75aafc7ae08240251e3bd3b4c1e6888
lukvidator <vlad.muhin1999@mail.ru>
2019-09-26 14:07:15 +0300

    Add param method

--- Line.py
+++ Line.py
@@ -58,4 +58,5 @@ class Line:
         else:
             raise WrongDimensionException(f"Can't evaluate Line's equation value in {self.dim()} dimension")
 
-
+    def param(self, t):
+        return self._point + t*self._vector
