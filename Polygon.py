744ddb6a9b5bf10e6877f676312a0c786a4f7803
lukvidator <vlad.muhin1999@mail.ru>
2019-09-27 22:57:32 +0300

    Add ray_test method

--- Polygon.py
+++ Polygon.py
@@ -1,9 +1,12 @@
 from typing import List
 from Point import Point
 from Segment import Segment
+from Vector import Vector
 from tools import rectangle_test
 from numpy import sign, array
 from numpy.linalg import det
+import numpy as np
+import random as rnd
 
 
 class Polygon:
@@ -20,10 +23,7 @@ class Polygon:
 
     @staticmethod
     def _nf2(a: Point, b: Point, p: Point):
-        return det([
-            array(p) - array(a),
-            array(b) - array(a)
-        ])
+        return det([array(p) - array(a), array(b) - array(a)])
 
     @property
     def square(self):
@@ -31,8 +31,8 @@ class Polygon:
         :return: sign square of Polygon
         """
         result = 0
-        for i in range(1, len(self.points)-1):
-            result += 0.5*self._nf2(self.points[0], self.points[i+1], self.points[i])
+        for i in range(1, len(self.points) - 1):
+            result += 0.5 * self._nf2(self.points[0], self.points[i + 1], self.points[i])
 
         return result
 
@@ -54,9 +54,9 @@ class Polygon:
 
     @property
     def edges(self):
-        n = len(self.points)
-        result = [Segment([self.points[i], self.points[i+1]]) for i in range(n-1)]
-        result.append(Segment([self.points[n - 1], self.points[0]]))
+        n = len(self._points)
+        result = [Segment([self._points[i], self._points[i + 1]]) for i in range(n - 1)]
+        result.append(Segment([self._points[n - 1], self._points[0]]))
         return result
 
     @property
@@ -69,11 +69,11 @@ class Polygon:
         n = len(self.points) - 1
         begin_orient = self._nf2(self.points[n], self.points[0], self.points[1])
         for i in range(1, n):
-            current_orient = self._nf2(self.points[i-1], self.points[i], self.points[i+1])
+            current_orient = self._nf2(self.points[i - 1], self.points[i], self.points[i + 1])
             if sign(begin_orient) != sign(current_orient):
                 break
         else:
-            last_orient = self._nf2(self.points[n-1], self.points[n], self.points[0])
+            last_orient = self._nf2(self.points[n - 1], self.points[n], self.points[0])
             if sign(begin_orient) == sign(last_orient):
                 return 1
         return 0
@@ -82,7 +82,25 @@ class Polygon:
         return rectangle_test(self._points, point)
 
     def ray_test(self, point):
-        pass
+        f, phi = [1, rnd.uniform(0, np.pi)]
+        v = np.array([np.cos(phi), np.sin(phi)])
+
+        for edge in self.edges:
+            m = np.array([v, Vector(edge[1], edge[0])])
+            if np.linalg.det(m) == 0:
+                continue
+
+            t1, t2 = np.dot(Vector(point, edge[0]), np.linalg.inv(m))
+            if t1 > 0:    # if point on the ray, but not on the other side of it
+                if t2 in (0., 1.):    # if the ray shoot in the end point of the edge
+                    return self.ray_test(point)    # need to rerandom the ray
+                elif 0 < t2 < 1:    # if the ray shoot between end points of the edge
+                    f *= -1
+            elif t1 == 0.:    # if the ray intersect edge line right in the start of itself
+                if 0 <= t2 <= 1:    # and the intersection point lies on the edge
+                    return 0    # return case: point lies on the edge
+
+        return f
 
 # p = Polygon([
 #     Point([0, 0]),
